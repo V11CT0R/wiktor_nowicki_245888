6519155fe89af715dd4a3d542da55b42c63ba79d
victor009\victor <wiktor09112@wp.pl>
2022-10-03 18:48:21 +0200

    new function add

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
+int add(int a, int b) {
+    return a + b;
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << add(4,2) << std::endl;
     return 0;
 }
