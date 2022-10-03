52b6f8455763b11a5200aa68520ce3e380b2acec
victor009\victor <wiktor09112@wp.pl>
2022-10-03 18:50:12 +0200

    Second function

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 
+using namespace std;
 int add(int a, int b) {
     return a + b;
 }
+string link(string a, string b) {
+    return a + b;
+}
 int main() {
-    std::cout << "Hello, World!" << add(4,2) << std::endl;
+    std::cout << link("Wiktor","Nowicki") << "Hello, World!" << add(4,2) << std::endl;
     return 0;
 }
